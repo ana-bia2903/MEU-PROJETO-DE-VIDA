8317db14ac7e018f1e923901024ece79d61b2215
ana-bia2903 <106672400+ana-bia2903@users.noreply.github.com>
2024-04-11 08:41:31 -0300

    dgghk

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+function addGoal() {
+    var newGoalInput = document.getElementById("new-goal");
+    var newGoalText = newGoalInput.value;
+    if (newGoalText !== "") {
+        var goalList = document.getElementById("goal-list");
+        var newGoalItem = document.createElement("li");
+        newGoalItem.textContent = newGoalText;
+        goalList.appendChild(newGoalItem);
+        newGoalInput.value = "";
+    } else {
+        alert("Por favor, insira um objetivo válido.");
+    }
+}
 
